aad5b3d0027aa871fe8146bba795d565365dba0a
Eranda Hettiarachchi <Erandahettiarachchi@gmail.com>
2021-12-25 22:26:38 +0530

    Updates

---
+++ main.js
@@ -0,0 +1,31 @@
+// Credentials
+const API_KEY = "AIzaSyAG2d9eQ7DQwhSSzJwy7zNPSz1H8vmhq78"
+const CHANNEL_ID = "UC3iCUuSl4NWxAcVgsS7PIlQ"
+const MAX_RESULT = 16
+const URL = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${CHANNEL_ID}&part=snippet,id&order=date&maxResults=${MAX_RESULT}`
+
+
+// DOM Elements
+const videoShowcase = document.getElementById("videos")
+const year = document.getElementById('year')
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const res = await fetch(URL)
+    const data = await res.json()
+    videoShowcase.innerHTML = ""
+
+    data.items.forEach((video) => {
+        videoShowcase.innerHTML += `
+        <a href="https://www.youtube.com/watch?v=${video.id.videoId}" target="_blank">
+            <div class="video" style="background-image: url(${video.snippet.thumbnails.high.url})"></div>
+        </a>
+    `
+    })
+
+})
+
+
+window.addEventListener('load', () => {
+    const now = new Date()
+    year.innerHTML = now.getFullYear()
+})
